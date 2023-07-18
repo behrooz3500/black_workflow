0bb45e22e2648b680d851b66ff51ed18a620a65b
behrooz3500 <behrooz3500@users.noreply.github.com>
2023-07-18 12:46:11 +0000

    Format code with black

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 name = "ddbehtooz"
-numbers = [
-            1,2,3
-        ]
+numbers = [1, 2, 3]
 
 for i in numbers:
     print(numbers)
 
 print("hello")
 
-temp =("dfd"   ,  "dfasdfadf")
+temp = ("dfd", "dfasdfadf")
