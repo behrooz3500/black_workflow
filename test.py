de4c85372b3f0180f40d21f6d02349dc6ccf8acd
kavehkm <mehrbaniankaveh@gmail.com>
2023-07-18 18:46:09 +0330

    update test.py: add some test code for blackify

--- test.py
+++ test.py
@@ -7,3 +7,9 @@ for i in numbers:
 print("hello")
 
 temp = ("dfd", "dfasdfadf")
+
+
+
+print('hello world, i want test blackify!')
+
+this_is_list = [1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'f', 'g', 'h']
